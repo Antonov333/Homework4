e99c76b3890dc0113ddd767c926daefd9b4edbc4
Antonov333 <antonovsergei333@yahoo.com>
2023-02-21 14:12:25 +0300

    Home Work #4 Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Just started my Homework with Java conditional operators ))");
+    }
+
+}
